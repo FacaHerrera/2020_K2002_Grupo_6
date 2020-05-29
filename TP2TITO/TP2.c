64f2c7097b10f2dad0ff20555cd126918ce39c50
agusfila <agusfila@gmail.com>
2020-05-28 22:24:41 -0300

    ok

--- TP2TITO/TP2.c
+++ TP2TITO/TP2.c
@@ -51,7 +51,7 @@ void escanearExpresionYescribirResultados()
     struct estado est;
     Pila pila = NULL;
     push(&pila,'$');
-    struct estado tablaTransicion[4][2][6] = {{{{3,ERROR2},{1,'$'}   ,{3,ERROR3},{0,'R'}   ,{3,ERROR3},{3,ERROR1}},{{3,ERROR2},{1,'$'}   ,{3,ERROR4},{0,'R'}   ,{3,ERROR3},{3,ERROR1}}},
+    struct estado tablaTransicion[4][2][6] = {{{{3,ERROR2},{1,'$'}   ,{3,ERROR3},{0,'R'}   ,{3,ERROR3},{3,ERROR1}},{{3,ERROR2},{1,'$'}   ,{3,ERROR3},{0,'R'}   ,{3,ERROR3},{3,ERROR1}}},
                                               {{{1,'$'}   ,{1,'$'}   ,{0,'$'}   ,{3,ERROR6},{3,ERROR5},{3,ERROR1}},{{1,'$'}   ,{1,'$'}   ,{0,'$'}   ,{3,ERROR6},{2,'N'}   ,{3,ERROR1}}},
                                               {{{3,ERROR7},{3,ERROR7},{0,'$'}   ,{3,ERROR6},{3,ERROR5},{3,ERROR1}},{{3,ERROR7},{3,ERROR7},{0,'$'}   ,{3,ERROR6},{2,'N'}   ,{3,ERROR1}}},
                                               {{{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}}   ,{{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}   ,{3,'I'}}}};
@@ -131,7 +131,7 @@ void escanearExpresionYescribirResultados()
             }
             if(determinarCima(cima) == 1 && estadoActual == 1)
             {
-                printf("^>ERROR: Falta ')'.\n");
+                printf(" ^>ERROR: Falta ')'.\n");
             }
             else if(determinarCima(cima) == 1 && estadoActual == 0)
             {
